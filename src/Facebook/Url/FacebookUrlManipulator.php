e9a5379c2a26ac03d163596df631ff3062d18562
Andrea Mancino <andrea.mancino@spreaker.com>
2025-07-22 11:13:18 +0200

    One more fix

--- src/Facebook/Url/FacebookUrlManipulator.php
+++ src/Facebook/Url/FacebookUrlManipulator.php
@@ -106,7 +106,7 @@ class FacebookUrlManipulator
      */
     public static function getParamsAsArray($url)
     {
-        $query = parse_url($url, PHP_URL_QUERY);
+        $query = parse_url($url ?? '', PHP_URL_QUERY);
         if (!$query) {
             return [];
         }
